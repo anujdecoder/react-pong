0024ab33de04f8beb5a501c6f0440c1c210b01a9
Anuj <anupta45@gmail.com>
2023-08-18 21:21:33 +0530

    feat: basic ball logic

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -6,15 +6,25 @@ import Ball from "./Ball"
 
 const Table: React.FC = () => {
   const [state, setState] = React.useState(0)
-  const handleMovement = (e: React.MouseEvent) => {
-    setState(e.clientY)
-  }
+  const [{ top, left }, setBallPosition] = React.useState({
+    top: 160,
+    left: 160,
+  })
+
+  React.useEffect(() => {
+    setInterval(() => {
+      setBallPosition(o => ({ top: o.top + 10, left: o.left + 10 }))
+    }, 100)
+  }, [])
+
+  const handleMovement = (e: React.MouseEvent) => setState(e.clientY)
+
   return (
     <div className="table" onMouseMove={handleMovement}>
       <PlayerPaddle mousePosition={state} topBorder={88} bottomBorder={window.innerHeight - 68} />
       <div className="net" />
       <div className={clsx("paddle", "computer")} />
-      <Ball top={160} left={160} />
+      <Ball top={top} left={left} />
     </div>
   )
 }
